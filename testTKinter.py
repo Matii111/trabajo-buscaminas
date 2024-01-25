bd69d5718355cfe941abe024d9a393a8327a6636
Matii111 <m.chaparrodaz@uandresbello.edu>
2024-01-25 19:38:54 -0300

    feat(major): Funcion para verificar victoria, opcion para comenzar nueva partida funcionando.

--- testTKinter.py
+++ testTKinter.py
@@ -5,8 +5,7 @@ from tkinter import ttk, messagebox
 class App(tk.Tk):        
 
     # funcion para iniciar un nuevo juego
-    def crear_tablero_ui(self, rows, cols):
-        
+    def crear_tablero_ui(self, rows, cols):                
         for i in range(rows):
             self.rowconfigure(i, weight=1)
         for j in range(cols):
@@ -29,7 +28,7 @@ class App(tk.Tk):
                     button.bind('<Button-3>', lambda event, row=i, col=j: self.handle_button_click_right(row, col, rows, verificar_bombas, event))
 
                 if (j > 0 and i == 0):
-                    button = ttk.Button(self, text=j, command=lambda row=i, col=j: self.handle_button_click_right(row, col, rows, verificar_bombas))
+                    button = ttk.Button(self, text=j, command=lambda row=i, col=j: self.handle_button_click(row, col, rows, verificar_bombas))
                     button.bind('<Button-3>', lambda event, row=i, col=j: self.handle_button_click_right(row, col, rows, verificar_bombas, event))
                 button.grid(row=i, column=j, sticky='nsew')
                 self.buttons[i][j] = button  
@@ -40,31 +39,44 @@ class App(tk.Tk):
     #da estilo a los botones clickeados
     def set_button_value(self, row, col, rows, board_content):
         self.buttons[row][col]['text'] = board_content
+
         if(board_content == 'unSafe'):
                 self.buttons[row][col]['text'] = ''
         if (board_content != 'safe' and board_content != 'unSafe' and board_content != ''):        
             self.buttons[row][col]['state'] = 'disabled'
-            self.buttons[row][col]['style'] = 'TButton'        
-        print('>> Posicion seleccionada: {},{}'.format(chr(64+row), col))
+            self.buttons[row][col]['style'] = 'TButton'
+            self.contador_mov -= 1  
+            print('>> Movimientos restantes: {}'.format(self.contador_mov))
+            print('>> Posicion seleccionada: {},{}'.format(chr(64+row), col))
+            if (self.contador_mov == 0):
+                print('--FIN DEL JUEGO--')
+                print('!Ganaste!')                
+                messagebox.showinfo('Buscaminas', 'Haz terminado el tablero, ¡Felicidades!')
+                messagebox.askquestion('Buscaminas', 'Quieres jugar otro tablero?') 
+                self.crear_tablero_ui(rows, rows)
+                self.contador_mov = int(((rows)-2**2)-(((rows)-2**2)*0.10))    
+        
 
     #reemplaza botones de tablero por valores de tablero con bombas
-    def handle_button_click(self, row, col, rows, tablero):
+    def handle_button_click(self, row, col, rows, tablero):                
         empty_button = self.buttons[row][col]['text']                                
-        board_content = ''
+        board_content = ''        
         if (empty_button == '' ):
             board_content = str(tablero[row][col])        
         if (board_content == '*'):
             print('--FIN DEL JUEGO--')             
             messagebox.showinfo('¡Perdiste!', 'Has tocado una bomba. ¡Fin del juego!')
             self.crear_tablero_ui(rows, rows)
+            self.contador_mov = int(((rows)-2**2)-(((rows)-2**2)*0.10))            
         
         if (row > 0 and col > 0 and board_content != '*'):
-            self.set_button_value(row, col, rows, board_content)        
+            self.set_button_value(row, col, rows, board_content)                    
     
     #marca banderas
     def handle_button_click_right(self, row, col, rows, tablero, event):
         empty_button = self.buttons[row][col]['text']                                
-        board_content = ''
+        board_content = ''        
+
         if (empty_button == '' ):
             board_content = 'safe'
 
@@ -75,7 +87,7 @@ class App(tk.Tk):
             self.set_button_value(row, col, rows, board_content) 
 
         if (event.num == 3):
-            print('bandera')
+            print('Bomba marcada en: {},{}'.format(chr(64+row), col))
             
     #genera bombas en posiciones aleatorias
     def bombas(self, size):
@@ -149,7 +161,9 @@ class App(tk.Tk):
         self.crear_tablero_ui(rows, cols)
         self.style = ttk.Style(self)
         self.style.configure('TButton', font=('Helvetica', 20))                                    
-    
+        self.contador_mov = int((((rows)-1)**2)-(((rows)-1)**2)*0.10)
+        print('asdadasda',self.contador_mov)
+
 if __name__ == '__main__':
     app = App(7, 7)
     
